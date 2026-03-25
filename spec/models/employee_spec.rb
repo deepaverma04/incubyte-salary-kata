b6b8094c4a0d70168271c5c0382647edc8d87e4e
deepa verma <deeparorverma@gmail.com>
2026-03-25 18:27:58 +0530

    Red: Add failing Employee model validation tests

---
+++ spec/models/employee_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Employee, type: :model do
+  it { should validate_presence_of(:full_name) }
+  it { should validate_presence_of(:job_title) }
+  it { should validate_presence_of(:country) }
+  it { should validate_presence_of(:salary) }
+  it { should validate_numericality_of(:salary).is_greater_than(0) }
+end
