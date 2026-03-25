8bd2ad111045f2c7aee676650e38ce54ee566e5a
deepa verma <deeparorverma@gmail.com>
2026-03-25 18:49:40 +0530

    Green: Implement Employee CRUD controller and routes

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,43 @@
+class EmployeesController < ApplicationController
+  before_action :set_employee, only: [:show, :update, :destroy]
+
+  def index
+    render json: Employee.all
+  end
+
+  def show
+    render json: @employee
+  end
+
+  def create
+    employee = Employee.new(employee_params)
+    if employee.save
+      render json: employee, status: :created
+    else
+      render json: employee.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @employee.update(employee_params)
+      render json: @employee
+    else
+      render json: @employee.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @employee.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_employee
+    @employee = Employee.find(params[:id])
+  end
+
+  def employee_params
+    params.require(:employee).permit(:full_name, :job_title, :country, :salary)
+  end
+end
